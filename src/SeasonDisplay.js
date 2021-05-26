364632e0f05360075dbb95b7688c42fc618763ea
William Putra <william.putra@viseo.com>
2021-05-26 12:16:55 +0700

    26 May 2021

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,36 @@
+import './seasonDisplay.css';
+import React from "react";
+
+const seasonConfig = {
+  summer: {
+    text: "Let's hit the beach!",
+    iconName: "sun"
+  },
+  winter: {
+    text: "Burr it is chilly",
+    iconName: "winter"
+  }
+}
+
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
+};
+
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const { text, iconName } = seasonConfig[season];
+
+  return (
+    <div className={`season-display ${season}`}>
+      <i className={`${iconName} icon-left massive icon`}/>
+      <h1>{text}</h1>
+      <i className={`${iconName} icon-right massive icon`}/>
+    </div>
+  );
+};
+
+export default SeasonDisplay;
